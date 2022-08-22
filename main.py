8fc09c025c71310fed1a856f9305514c1057d55b
Froan <54677852+fromann@users.noreply.github.com>
2022-08-22 22:03:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
 def get_time():
-  datetime获取北京时间
   BJ_tz = timezone(timedelta(hours=8), 'Asia/Shanghai')
   dictDate={'Monday':'星期一','Tuesday':'星期二','Wednesday':'星期三','Thursday':'星期四','Friday':'星期五','Saturday':'星期六','Sunday':'星期天'}
   a=dictDate[datetime.now(BJ_tz).strftime('%A')]
